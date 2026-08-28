4537ef9d1877be9d7af46a29077f6cee1edd993b
Edd <mrpotato387@gmail.com>
2026-08-28 15:06:17 -0400

    Update system.js

--- frontend/system.js
+++ frontend/system.js
@@ -6,7 +6,7 @@ let tanques = JSON.parse(localStorage.getItem("tanques")) || [];
 mostrarTanques();
 
 formulario.addEventListener("submit", function (event) {
-    event.preventDefault();
+event.preventDefault();
 
 const id = document.getElementById("id").value.trim();
 const nombre = document.getElementById("nombre").value.trim();
@@ -30,21 +30,23 @@ const nuevoTanque = {
 tanques.push(nuevoTanque);
 
 localStorage.setItem("tanques", JSON.stringify(tanques));
-mostrarTanques();
-formulario.reset();
 
+mostrarTanques();
+
+formulario.reset();
 });
 
 function mostrarTanques() {
+
 
 tablaTanques.innerHTML = "";
 
 if (tanques.length === 0) {
     tablaTanques.innerHTML = `
-        < tr >
-        <td colspan="5">No hay tanques registrados.</td>
-        </tr >
-        `;
+        <tr>
+            <td colspan="5">No hay tanques registrados.</td>
+        </tr>
+    `;
 
     return;
 }
@@ -54,7 +56,7 @@ tanques.forEach(function (tanque) {
     const fila = document.createElement("tr");
 
     fila.innerHTML = `
-        < td > ${ tanque.id }</td >
+        <td>${tanque.id}</td>
         <td>${tanque.nombre}</td>
         <td>${tanque.generacion}</td>
         <td>${tanque.pais}</td>
